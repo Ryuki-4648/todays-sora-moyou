044b5bf22d0805acf1269fb4c84d6ad0fbc1bfd1
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-09-30 23:54:48 +0900

    フォントとロゴ追加、style修正

--- src/api/nasa-yesterday-api.js
+++ src/api/nasa-yesterday-api.js
@@ -4,20 +4,20 @@ const NASA_API_KEY = 'ZQKrkabQ5a9mlgFzWDcQJdjXHUPlEJej3TQhO14D';
 
 const getYesterdayDate = () => {
   const today = new Date();
-  const yesterday = newDate(today);
+  const yesterday = new Date(today);
   yesterday.setDate(today.getDate() - 1); // todayをコピーして1日前に設定
   const year = yesterday.getFullYear();
   const month = String(yesterday.getMonth() + 1).padStart(2, '0'); // 1桁の月を2桁のゼロパディングされた文字列に変換
   const day = String(yesterday.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
-}
+};
 
 export async function fetchNasaYesterdayApiData() {
   try {
     const yesterdayResponse = await axios.get(
       `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}&date=${getYesterdayDate}`
     );
-
+    
     const { title, url } = yesterdayResponse.data;
     return { title, url };
 
@@ -25,4 +25,4 @@ export async function fetchNasaYesterdayApiData() {
     console.error('NASA APIリクエストエラー:', error);
     return { title: '', url: '' };
   }
-}
+};
