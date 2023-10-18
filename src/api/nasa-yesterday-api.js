f1f35e25a90c0efa78f1a2118272aebf7239084c
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-10-19 00:00:08 +0900

    修正

--- src/api/nasa-yesterday-api.js
+++ src/api/nasa-yesterday-api.js
@@ -5,7 +5,7 @@ const NASA_API_KEY = 'ZQKrkabQ5a9mlgFzWDcQJdjXHUPlEJej3TQhO14D';
 const getYesterdayDate = () => {
   const today = new Date();
   const yesterday = new Date(today);
-  yesterday.setDate(today.getDate() - 1); // todayをコピーして1日前に設定
+  yesterday.setDate(today.getDate() + 1); // todayをコピーして1日前に設定
   const year = yesterday.getFullYear();
   const month = String(yesterday.getMonth() + 1).padStart(2, '0'); // 1桁の月を2桁のゼロパディングされた文字列に変換
   const day = String(yesterday.getDate()).padStart(2, '0');
